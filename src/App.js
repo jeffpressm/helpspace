a96076a4d208951243ba1586061ed6c4b0b54066
Tim Hettler <hettler@work.co>
2020-04-13 12:41:08 -0400

    switch sheet id, print out user info

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import Home from './Home';
 import NeedHelp from './NeedHelp';
 import User from './User';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import useTabletop from './utils/hooks/useTabletop';
 
 function App() {
+  const tdata = useTabletop('1SMus2rG-kjfy2SXASC-V8trxB4BFF7ITx-QvaoBOags');
+
+  console.log(tdata);
   return (
     <Router>
       <Switch>
--- src/User/index.js
+++ src/User/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import useTabletop from '../utils/hooks/useTabletop';
 
 const User = () => {
+  const tData = useTabletop('1SMus2rG-kjfy2SXASC-V8trxB4BFF7ITx-QvaoBOags');
+  const [userData, setUserData] = useState();
   const { userId } = useParams();
 
-  // Here we can use the userId to query the Spreadsheet for user data
+  useEffect(() => {
+    if (!tData) {
+      return;
+    }
 
-  return <div>Thanks for Guilding, {userId}</div>;
+    const newUserData = tData.find((c) => c['ID'] === userId);
+
+    setUserData(newUserData);
+  }, [tData, setUserData, userId]);
+
+  if (!userData) {
+    // TODO: loading indicator
+    return null;
+  }
+
+  const { Name: name, Topic: topic, Email: email } = userData;
+
+  return (
+    <div>
+      Thanks for Guilding, {name}. We'll email you at {email} when we've found
+      an {topic} expert to help you.
+    </div>
+  );
 };
 
 export default User;
