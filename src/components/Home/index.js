d0b7ff153b647eb33dc053aa78e88bd7e4e45545
Tim Hettler <hettler@work.co>
2020-04-16 14:34:53 -0400

    use cms data for hero, add type mixins

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
 import { ReactComponent as ArrowIcon } from 'assets/icons/arrow.svg';
 import { RouteList } from 'lib/routes';
+import { SpreadsheetContext } from 'utils/context/SpreadsheetContextProvider';
 
 import styles from './Home.module.scss';
 
 const cx = classNames.bind(styles);
 
 const Home = () => {
+  const { content } = useContext(SpreadsheetContext);
+  const { Hero: HeroContent } = content;
+
   return (
     <article className={cx('root')}>
       <section className={cx('section', 'get')}>
         <Link className={cx('link')} to={RouteList.get}>
-          <h2 className={cx('title')}>Get help</h2>
-          <div className={cx('copy')}>
-            We all need help or know small businesses that do. Get operational
-            advice for your most pressing challenges. Pay it forward if you can.
-          </div>
+          <h2 className={cx('title')}>{HeroContent[0]['CTA']}</h2>
+          <div className={cx('copy')}>{HeroContent[0]['Body']}</div>
           <div className={cx('cta')}>
             <ArrowIcon />
           </div>
@@ -26,12 +27,8 @@ const Home = () => {
       </section>
       <section className={cx('section', 'give')}>
         <Link className={cx('link')} to={RouteList.give}>
-          <h2 className={cx('title')}>Give help</h2>
-          <div className={cx('copy')}>
-            We all have skills that can help others during the crisis. Give
-            operational advice to small businesses in need. Share whatever
-            expertise you have.
-          </div>
+          <h2 className={cx('title')}>{HeroContent[1]['CTA']}</h2>
+          <div className={cx('copy')}>{HeroContent[1]['Body']}</div>
           <div className={cx('cta')}>
             <ArrowIcon />
           </div>
