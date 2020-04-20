dcdcb3cb576fab4b18c8f1cf522ef254c3627945
Tim Hettler <hettler@work.co>
2020-04-20 15:22:08 -0400

    add faq illustration, update design

--- src/components/Faq/index.js
+++ src/components/Faq/index.js
@@ -5,6 +5,7 @@ import Footer from 'components/Footer';
 import { SpreadsheetContext } from 'utils/context/SpreadsheetContextProvider';
 
 import TwoUp from 'components/TwoUp';
+import heroImg from './assets/hero.png';
 import styles from './Faq.module.scss';
 
 const cx = classNames.bind(styles);
@@ -23,35 +24,46 @@ const Faq = () => {
               <div className={cx('hero-body')}>{HeroContent[0]['Body']}</div>
             </div>
           }
-          slot2={<div></div>}
+          slot2={
+            <div className={cx('hero-img')}>
+              <img src={heroImg} alt="" />
+            </div>
+          }
         />
         <div className={cx('faq-container')}>
-          <ul>
-            {FaqContent.map((item) => {
-              const {
-                Question: question,
-                Answer: answer,
-                Description: description,
-              } = item;
-              return (
-                <div key={question} className={cx('faq-item')}>
-                  <TwoUp
-                    slot1={<h3 className={cx('faq-headline')}>{question}</h3>}
-                    slot2={
-                      <>
-                        <div>
-                          <strong className={cx('faq-answer')}>{answer}</strong>
-                        </div>
-                        <div className={cx('faq-description')}>
-                          {description}
-                        </div>
-                      </>
-                    }
-                  />
-                </div>
-              );
-            })}
-          </ul>
+          <div className={cx('faq-header')}>
+            <h3>FAQs</h3>
+          </div>
+          <div className={cx('faq-body')}>
+            <ul>
+              {FaqContent.map((item) => {
+                const {
+                  Question: question,
+                  Answer: answer,
+                  Description: description,
+                } = item;
+                return (
+                  <div key={question} className={cx('faq-item')}>
+                    <TwoUp
+                      slot1={<h3 className={cx('faq-headline')}>{question}</h3>}
+                      slot2={
+                        <>
+                          <div>
+                            <strong className={cx('faq-answer')}>
+                              {answer}
+                            </strong>
+                          </div>
+                          <div className={cx('faq-description')}>
+                            {description}
+                          </div>
+                        </>
+                      }
+                    />
+                  </div>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       </article>
       <footer className={cx('footer')}>
