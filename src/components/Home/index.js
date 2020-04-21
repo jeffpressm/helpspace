ab1177c4390e0bed3d3c2193eeed483601c729f5
Tim Hettler <hettler@work.co>
2020-04-21 15:13:32 -0400

    update hero layout

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,57 +1,20 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import classNames from 'classnames/bind';
 
-import { ReactComponent as ArrowIcon } from 'assets/icons/arrow.svg';
 import About from 'components/About';
 import Footer from 'components/Footer';
-import { RouteList } from 'lib/routes';
-import { SpreadsheetContext } from 'utils/context/SpreadsheetContextProvider';
 
-import getImg from './assets/get.png';
-import giveImg from './assets/give.png';
+import Hero from './Hero';
 import styles from './Home.module.scss';
 
 const cx = classNames.bind(styles);
 
 const Home = () => {
-  const { content } = useContext(SpreadsheetContext);
-  const { Hero: HeroContent } = content;
-
   return (
     <>
-      <article className={cx('root')}>
-        <section className={cx('section', 'get')}>
-          <Link className={cx('link')} to={RouteList.get}>
-            <div className={cx('content-container')}>
-              <h2 className={cx('title')}>{HeroContent[0]['CTA']}</h2>
-              <div className={cx('copy')}>{HeroContent[0]['Body']}</div>
-              <div className={cx('cta')}>
-                <ArrowIcon />
-              </div>
-              <div className={cx('image', 'image--get')}>
-                <img role="presentation" src={getImg} alt="" />
-              </div>
-            </div>
-          </Link>
-        </section>
-        <section className={cx('section', 'give')}>
-          <Link className={cx('link')} to={RouteList.give}>
-            <div className={cx('content-container')}>
-              <h2 className={cx('title')}>{HeroContent[1]['CTA']}</h2>
-              <div className={cx('copy')}>{HeroContent[1]['Body']}</div>
-              <div className={cx('cta')}>
-                <ArrowIcon />
-              </div>
-              <div className={cx('image', 'image--give')}>
-                <img role="presentation" src={giveImg} alt="" />
-              </div>
-            </div>
-          </Link>
-        </section>
-      </article>
+      <Hero />
       <About />
-      <footer className={cx('footer')}>
+      <footer className={cx('footer-container')}>
         <Footer />
       </footer>
     </>
