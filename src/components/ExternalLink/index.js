1f65162fdc72ab6f08fd4607a41e76f24cf97d54
Tim Hettler <hettler@work.co>
2020-05-08 15:15:24 -0400

    response spacing

--- src/components/ExternalLink/index.js
+++ src/components/ExternalLink/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { OutboundLink } from 'react-ga';
 
-const ExternalLink = ({ href, children, rest }) => (
+const ExternalLink = ({ href, children, ...rest }) => (
   <OutboundLink
     eventLabel={href}
     to={href}
     target="_blank"
     rel="noopener noreferrer"
+    {...rest}
   >
     {children}
   </OutboundLink>
